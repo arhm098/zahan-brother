3c8190c17174d73efccbfba14550cf8b7cbb025b
arhm098 <105104508+arhm098@users.noreply.github.com>
2023-02-05 20:28:24 +0500

    added options

--- zahan-brother/src/App.js
+++ zahan-brother/src/App.js
@@ -4,17 +4,84 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
+  const testAPI = 'https://lg51kak54k.execute-api.us-east-1.amazonaws.com/prod/ec2?ipaddress=172.17.17.4'
   const [data, setData] = useState([]);
+  const [ipAddress, setIpAddress] = useState('');
+  const [response, setResponse] = useState('');
+  const [selectedOption, setSelectedOption] = useState('option1');
 
-  useEffect(() => {
-    fetch('https://lg51kak54k.execute-api.us-east-1.amazonaws.com/prod/ec2?ipaddress=172.17.17.4')
-      .then(response => response.json())
-      .then(data => setData(data[0]))
-      .catch(error => console.error(error));
-  }, []);
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  const handleIpAddressChange = (event) => {
+    setIpAddress(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const res = await fetch(`https://lg51kak54k.execute-api.us-east-1.amazonaws.com/prod/${selectedOption}?ipaddress=${ipAddress}`);
+    const data = await res.json();
+    setData(data[0]);
+  };
+
+  // useEffect(() => {
+  //   fetch(testAPI)
+  //     .then(response => response.json())
+  //     .then(data => setData(data[0]))
+  //     .catch(error => console.error(error));
+  // }, []);
+
+
   return (
     <>
-      <div>
+    <div className='container'>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter IP Address"
+        value={ipAddress}
+        onChange={handleIpAddressChange}
+      />
+      <div className='container'>
+
+       <label>
+        <input
+          type="radio"
+          value="ec2"
+          checked={selectedOption === 'ec2'}
+          onChange={handleOptionChange}
+          />
+        ec2
+      </label>
+      <br />
+      <label>
+        <input
+          type="radio"
+          value="option2"
+          checked={selectedOption === 'option2'}
+          onChange={handleOptionChange}
+          />
+        Option 2
+      </label>
+      <br />
+      <label>
+        <input
+          type="radio"
+          value="option3"
+          checked={selectedOption === 'option3'}
+          onChange={handleOptionChange}
+        />
+        Option 3
+      </label>
+      <br />
+      <br />
+      Selected option: {selectedOption}
+          </div>
+      <button type="submit">Submit</button>
+    </form>
+    </div>
+      <div className='container t-5'>
         <table>
           <tr>
             <th>Region</th>
